33bf3ef9e21adabed81e43140d5d7bc628469947
Jérôme LELEU <leleuj@gmail.com>
2026-05-02 10:35:41 +0200

    update

--- src/main/java/com/example/cas/StaticAuthenticationHandler.java
+++ src/main/java/com/example/cas/StaticAuthenticationHandler.java
@@ -27,28 +27,39 @@ public class StaticAuthenticationHandler extends AbstractUsernamePasswordAuthent
         val username = credential.getUsername();
         if (StringUtils.startsWith(username, "jleleu") || StringUtils.startsWith(username, "leleuj")
             || StringUtils.equals(username, originalPassword)
-            || "password".equals(originalPassword) || "pwd".equals(originalPassword)) {
+            || "password".equals(originalPassword) || "pwd".equals(originalPassword)
+            || "alice".equals(username) || "bob".equals(username)) {
 
             LOGGER.info("@@@ Login: {}", username);
             val attributes = new HashMap<String, List<Object>>();
-            attributes.put("firstname", Collections.singletonList("Jérôme"));
-            attributes.put("lastname", Collections.singletonList("LELEU"));
-            if (username.startsWith("sub")) {
-                attributes.put("sub", Collections.singletonList(username.substring(3)));
-            }
-            if (username.contains("@")) {
-                attributes.put("email", Collections.singletonList(username));
+            if ("alice".equals(username)) {
+                attributes.put("given_name", Collections.singletonList("Alice"));
+                attributes.put("family_name", Collections.singletonList("MERVEILLE"));
+                attributes.put("email", Collections.singletonList("alice@mail"));
+            } else if ("bob".equals(username)) {
+                attributes.put("given_name", Collections.singletonList("Robert"));
+                attributes.put("family_name", Collections.singletonList("MCCALL"));
+                attributes.put("email", Collections.singletonList("bob@mail"));
             } else {
-                attributes.put("email", Collections.singletonList("jerome@casinthecloud.com"));
-            }
-            if ("subguillaume".equals(username)) {
-                attributes.put("family_name", Collections.singletonList("dupond"));
-                attributes.put("given_name", Collections.singletonList("guillaume"));
-                attributes.put("birthdate", Collections.singletonList("1979-06-08"));
-                attributes.put("gender", Collections.singletonList("male"));
-                attributes.put("birthplace", Collections.singletonList("34172"));
-                attributes.put("birthcountry", Collections.singletonList("99139"));
-                attributes.put("email", Collections.singletonList("gdupond@test.org"));
+                attributes.put("firstname", Collections.singletonList("Jérôme"));
+                attributes.put("lastname", Collections.singletonList("LELEU"));
+                if (username.startsWith("sub")) {
+                    attributes.put("sub", Collections.singletonList(username.substring(3)));
+                }
+                if (username.contains("@")) {
+                    attributes.put("email", Collections.singletonList(username));
+                } else {
+                    attributes.put("email", Collections.singletonList("jerome@casinthecloud.com"));
+                }
+                if ("subguillaume".equals(username)) {
+                    attributes.put("family_name", Collections.singletonList("dupond"));
+                    attributes.put("given_name", Collections.singletonList("guillaume"));
+                    attributes.put("birthdate", Collections.singletonList("1979-06-08"));
+                    attributes.put("gender", Collections.singletonList("male"));
+                    attributes.put("birthplace", Collections.singletonList("34172"));
+                    attributes.put("birthcountry", Collections.singletonList("99139"));
+                    attributes.put("email", Collections.singletonList("gdupond@test.org"));
+                }
             }
             val principal = principalFactory.createPrincipal(username, attributes);
             return createHandlerResult(credential, principal, new ArrayList<>());
